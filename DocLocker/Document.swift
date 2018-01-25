e8d87a6eb94aa03a9e044d0fb7f6aaede5f98801
Natash Bangera <natashnb@gmail.com>
2018-01-25 17:48:14 +0530

    Initial Commit

---
+++ DocLocker/Document.swift
@@ -0,0 +1,19 @@
+//
+// Created by Natash Bangera on 1/25/18.
+// Copyright © 2018 Natash Bangera. All rights reserved.
+// 
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
